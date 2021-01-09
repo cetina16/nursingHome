85e46acc78615a4a78f056b0fd80de8de169525a
cetina16 <cetina16@itu.edu.tr>
2021-01-10 00:27:01 +0300

    interface changes

--- views.py
+++ views.py
@@ -385,12 +385,84 @@ def signup_page():
     else:
         db = MySQLdb.connect(host="localhost", user="root", passwd="1616", db="db_nursing")
         cursor = db.cursor()
-        #cursor.execute("DROP TABLE IF EXISTS Doctor")
-       # cursor.execute("DROP TABLE IF EXISTS Resident")
-       # cursor.execute ("DROP TABLE IF EXISTS Nurse")
-       # cursor.execute("DROP TABLE IF EXISTS Disease")
-       # cursor.execute("DROP TABLE IF EXISTS Nursinghome")
-       
+        cursor.execute("DROP TABLE IF EXISTS Diseaseowners")
+        cursor.execute("DROP TABLE IF EXISTS Resident")
+        cursor.execute("DROP TABLE IF EXISTS Nurse")
+        cursor.execute("DROP TABLE IF EXISTS Disease")
+        cursor.execute("DROP TABLE IF EXISTS Doctor")
+        cursor.execute("DROP TABLE IF EXISTS Nursinghome")
+        
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Nursinghome(homeid INT AUTO_INCREMENT,
+                        name VARCHAR(40) NOT NULL,
+                        city VARCHAR(40) NOT NULL,
+                        type VARCHAR(40) NOT NULL,
+                        address VARCHAR(40) NOT NULL,
+                        tel VARCHAR(40) NOT NULL,
+                        PRIMARY KEY (homeid))""")
+        
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Doctor(doctorid INT AUTO_INCREMENT,
+                            email VARCHAR(40) UNIQUE,
+                            name VARCHAR (40) NOT NULL ,
+                            password VARCHAR (100)  NOT NULL ,
+                            nursinghomeid INT,
+                            FOREIGN KEY (nursinghomeid)  
+                            REFERENCES Nursinghome(homeid) 
+                            ON DELETE CASCADE
+                            ON UPDATE CASCADE,
+                            PRIMARY KEY (doctorid))""")
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Nurse(nurseid INT AUTO_INCREMENT,
+                        name VARCHAR(40) NOT NULL,
+                        capacity INT NOT NULL,
+                        capacity_exist INT,
+                        type VARCHAR(40) NOT NULL,
+                        address VARCHAR(90) NOT NULL,
+                        tel VARCHAR(40) NOT NULL,
+                        nursinghomeid  INT NOT NULL,
+                        FOREIGN KEY (nursinghomeid) 
+                        REFERENCES Nursinghome(homeid)
+                        ON DELETE CASCADE 
+                        ON UPDATE CASCADE,
+                        PRIMARY KEY (nurseid))""")
+        
+      
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Disease(diseaseid INT AUTO_INCREMENT,
+                        name VARCHAR(40) NOT NULL,
+                        risklevel INT NOT NULL,
+                        period VARCHAR(40) NOT NULL,
+                        homeid INT NOT NULL,
+                        FOREIGN KEY (homeid) 
+                        REFERENCES Nursinghome(homeid)
+                        ON DELETE CASCADE 
+                        ON UPDATE CASCADE,
+                        PRIMARY KEY (diseaseid))""")
+
+
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Resident(residentid INT AUTO_INCREMENT,
+                        name VARCHAR(40) NOT NULL,
+                        age INT NOT NULL,
+                        bedridden VARCHAR(40) NOT NULL,
+                        gender VARCHAR(20) NOT NULL,
+                        tel VARCHAR(40) NOT NULL,
+                        nurseid  INT NOT NULL,
+                        FOREIGN KEY (nurseid) 
+                        REFERENCES Nurse(nurseid)
+                        ON DELETE CASCADE 
+                        ON UPDATE CASCADE,
+                        PRIMARY KEY (residentid))""")
+        cursor.execute("""CREATE TABLE IF NOT EXISTS DiseaseOwners(residentid INT NOT NULL,
+                                diseaseid INT NOT NULL,
+                                FOREIGN KEY (residentid)	
+                                REFERENCES Resident(residentid)
+                                ON DELETE CASCADE 
+                                ON UPDATE CASCADE,
+                                FOREIGN KEY (diseaseid)	
+                                REFERENCES Disease (diseaseid)
+                                ON DELETE RESTRICT
+                                ON UPDATE CASCADE,
+                                startdate DATE NOT NULL,
+                                enddate DATE,
+                                PRIMARY KEY(residentid , diseaseid))""")
+     
         form_name = request.form["name"] 
         form_homename = request.form["homename"]
         form_city = request.form["city"]
@@ -426,57 +498,7 @@ def signup_page():
     
     
         
-        cursor.execute("""CREATE TABLE IF NOT EXISTS Nursinghome(homeid INT AUTO_INCREMENT,
-                        name VARCHAR(40) NOT NULL,
-                        city VARCHAR(40) NOT NULL,
-                        type VARCHAR(40) NOT NULL,
-                        address VARCHAR(40) NOT NULL,
-                        tel VARCHAR(40) NOT NULL,
-                        PRIMARY KEY (homeid))""")
-        cursor.execute("""CREATE TABLE IF NOT EXISTS Doctor(doctorid INT AUTO_INCREMENT,
-                            email VARCHAR(40) UNIQUE,
-                            name VARCHAR (40) NOT NULL ,
-                            password VARCHAR (100)  NOT NULL ,
-                            nursinghomeid INT,
-                            FOREIGN KEY (nursinghomeid)  
-                            REFERENCES Nursinghome(homeid) 
-                            ON DELETE CASCADE
-                            ON UPDATE CASCADE,
-                            PRIMARY KEY (doctorid))""")
-        cursor.execute("""CREATE TABLE IF NOT EXISTS Nurse(nurseid INT AUTO_INCREMENT,
-                        name VARCHAR(40) NOT NULL,
-                        capacity INT NOT NULL,
-                        type VARCHAR(40) NOT NULL,
-                        address VARCHAR(90) NOT NULL,
-                        tel VARCHAR(40) NOT NULL,
-                        nursinghomeid  INT NOT NULL,
-                        FOREIGN KEY (nursinghomeid) 
-                        REFERENCES Nursinghome(homeid)
-                        ON DELETE CASCADE 
-                        ON UPDATE CASCADE,
-                        PRIMARY KEY (nurseid))""")
-        cursor.execute("""CREATE TABLE IF NOT EXISTS Resident(residentid INT AUTO_INCREMENT,
-                        name VARCHAR(40) NOT NULL,
-                        age INT NOT NULL,
-                        bedridden VARCHAR(40) NOT NULL,
-                        gender VARCHAR(20) NOT NULL,
-                        tel VARCHAR(40) NOT NULL,
-                        nurseid  INT NOT NULL,
-                        FOREIGN KEY (nurseid) 
-                        REFERENCES Nurse(nurseid)
-                        ON DELETE CASCADE 
-                        ON UPDATE CASCADE,
-                        PRIMARY KEY (residentid))""")
-        cursor.execute("""CREATE TABLE IF NOT EXISTS Disease(diseaseid INT AUTO_INCREMENT,
-                        name VARCHAR(40) NOT NULL,
-                        risklevel INT NOT NULL,
-                        period VARCHAR(40) NOT NULL,
-                        homeid INT NOT NULL,
-                        FOREIGN KEY (homeid) 
-                        REFERENCES Nursinghome(homeid)
-                        ON DELETE CASCADE 
-                        ON UPDATE CASCADE,
-                        PRIMARY KEY (diseaseid))""")
+
         
         line1 = "INSERT INTO Nursinghome(name,city,type,address,tel) VALUES (%s,%s,%s,%s,%s)"
         data1 = (homename,city,type_,address,tel)
@@ -525,19 +547,7 @@ def login_page():
                 flash("You have logged in.")
                 db = MySQLdb.connect(host="localhost", user="root", passwd="1616", db="db_nursing")
                 cursor = db.cursor()
-                cursor.execute("""CREATE TABLE IF NOT EXISTS DiseaseOwners(residentid INT NOT NULL,
-                                diseaseid INT NOT NULL,
-                                FOREIGN KEY (residentid)	
-                                REFERENCES Resident(residentid)
-                                ON DELETE CASCADE 
-                                ON UPDATE CASCADE,
-                                FOREIGN KEY (diseaseid)	
-                                REFERENCES Disease (diseaseid)
-                                ON DELETE RESTRICT
-                                ON UPDATE CASCADE,
-                                startdate DATE NOT NULL,
-                                enddate DATE,
-                                PRIMARY KEY(residentid , diseaseid))""")
+               
                 db.commit()
                 cursor.close()
                 #return render_template("home.html",islogged=islogged,name=name,date=date_time)
