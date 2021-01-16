833f33195eb3f9ed44a5e15e9c3582727afed076
cetina16 <cetina16@itu.edu.tr>
2021-01-16 19:24:19 +0300

    commint try

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\asus\Desktop\nursingHome\venv
+set VIRTUAL_ENV=C:\Users\asus\Desktop\Yeni klasör\nursingHome\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
